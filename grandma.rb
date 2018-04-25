f60d247d4266bd850a90cfc16ff62d973d1ec910
Karl Amoguis <ktamoguis@gmail.com>
2018-04-25 09:08:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(greeting)
   if greeting == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif greeting == greeting.upcase
-    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
